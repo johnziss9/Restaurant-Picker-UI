7c8606331febf2d6131c31a2d17b5900c541cc57
John Z <johnziss9@gmail.com>
2021-02-09 11:57:12 +0000

    Added bootstrap and jquery. Added Homepage Login button and styled it.

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -6,6 +6,7 @@
          return (
             <div className="home-container">
                 {/* BUTTON/LOG IN GOES HERE AS COMPONENT */}
+                <button type="button" class="btn btn-dark btn-lg login">Login</button>
             </div>
          );
      }
