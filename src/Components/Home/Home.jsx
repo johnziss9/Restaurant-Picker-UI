70307738cab01bf5b6f11f16cb8e70d8b5225788
John Z <johnziss9@gmail.com>
2021-02-09 16:06:54 +0000

    Created a login form which appears when the login button is clicked and visa versa when Cancel button is clicked.

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,11 +2,42 @@
  import './Home.css';
 
  class Home extends React.Component {
+
+    state = {
+        isActive: true
+    }
+
+    handleShow = () => {
+        this.setState({
+            isActive: true        
+        })
+    }
+
+    handleHide = () => {
+        this.setState({
+            isActive: false
+        })
+    }
+
      render() {
          return (
             <div className="home-container">
-                {/* BUTTON/LOG IN GOES HERE AS COMPONENT */}
-                <button type="button" class="btn btn-dark btn-lg login">Login</button>
+                {this.state.isActive ?
+                <button type="button" className="btn btn-dark btn-lg login-btn" onClick={this.handleHide}>Login</button> :
+                <form>
+                    <div className="form-group">
+                        <input type="text" className="form-control" id="username" placeholder="Username" />
+                    </div>
+                    <div className="form-group">
+                        <input type="text" className="form-control" id="password" placeholder="Password" />
+                    </div>
+                    <div className="form-group">
+                        <button type="button" className="btn btn-danger login-form-btn" onClick={this.handleShow}>Cancel</button>
+                        <button type="button" className="btn btn-success login-form-btn">Login</button>
+                    </div>
+                </form> 
+                
+                }
             </div>
          );
      }
