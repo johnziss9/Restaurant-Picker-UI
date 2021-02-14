ecd4b0c2ea6c0d504b845d81deaaf6477e86f651
John Z <johnziss9@gmail.com>
2021-02-14 20:27:04 +0000

    Added login functionality by calling the login method in the auth controller and printing results in the console. Installed react-router-dom.

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,8 +3,17 @@
 
  class Home extends React.Component {
 
-    state = {
-        isActive: true
+    constructor(props) {
+        super(props);
+        this.state = {
+            isActive: true,
+            username: '',
+            password: ''
+        }
+
+        this.handleUsername = this.handleUsername.bind(this);
+        this.handlePassword = this.handlePassword.bind(this);
+        this.handleLogin = this.handleLogin.bind(this);
     }
 
     handleShow = () => {
@@ -19,6 +28,32 @@
         })
     }
 
+    handleUsername(event) {
+        this.setState({ username: event.target.value })
+    }
+
+    handlePassword(event) {
+        this.setState({ password: event.target.value })
+    }
+
+    handleLogin(event) {
+        fetch('https://localhost:5001/auth/Login', {
+            method: 'post',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: this.state.username,
+                password: this.state.password
+            })
+        })
+        .then((Response) => Response.json())
+        .then((result) => {
+            console.log(result);
+        })
+    }
+
      render() {
          return (
             <div className="home-container">
@@ -26,14 +61,14 @@
                 <button type="button" className="btn btn-dark btn-lg login-btn" onClick={this.handleHide}>Login</button> :
                 <form>
                     <div className="form-group">
-                        <input type="text" className="form-control" id="username" placeholder="Username" />
+                        <input type="text" className="form-control" id="username" placeholder="Username" onChange={this.handleUsername} />
                     </div>
                     <div className="form-group">
-                        <input type="text" className="form-control" id="password" placeholder="Password" />
+                        <input type="password" className="form-control" id="password" placeholder="Password" onChange={this.handlePassword}/>
                     </div>
                     <div className="form-group">
                         <button type="button" className="btn btn-light login-form-btn" onClick={this.handleShow}>Cancel</button>
-                        <button type="button" className="btn btn-dark login-form-btn">Login</button>
+                        <button type="button" className="btn btn-dark login-form-btn" onClick={this.handleLogin}>Login</button>
                     </div>
                 </form> }
 
