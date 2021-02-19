231b786eca39381ac866388d80b8c2725916d518
John Z <johnziss9@gmail.com>
2021-02-19 20:52:48 +0000

    Added images in View Restaurants and Pick Restaurant pages. Started styling View Restaurants page (not finished).

--- src/Components/ViewRestaurants/ViewRestaurants.jsx
+++ src/Components/ViewRestaurants/ViewRestaurants.jsx
@@ -1,6 +1,8 @@
 import moment from 'moment';
 import React from 'react';
 import './ViewRestaurants.css'
+import { Link } from 'react-router-dom';
+import { NavLink } from 'reactstrap';
 
 class ViewRestaurants extends React.Component {
 
@@ -23,16 +25,22 @@ class ViewRestaurants extends React.Component {
 
     render() {
         return (
-            <div className="container">
-                <div className="view-restaurants-container">
-                    {Array.isArray(this.state.restaurants.data) && this.state.restaurants.data.map( res => (
-                        <div className="restaurant-container" key={res.id}>
-                            <h4 className="restaurant-name">{res.name}</h4>
-                            <p className="restaurant-location">{res.location}</p>
-                            <p className="restaurant-cuisine">{res.cuisine}</p>
-                            <small className="restaurant-user-details">Added by {res.addedBy} on {moment(res.addedOn).format('MMMM Do YYYY')}</small>
-                        </div>
-                    ))}
+            <div className="view-restaurants-container">
+                <div className="container view-restaurants-header">
+                    <h1 className="view-restaurants-title">Pick Restaurant</h1>
+                    <NavLink className="view-restaurants-go-back" tag={Link} to="/Menu">Main Menu</NavLink>
+                </div>
+                <div className="view-restaurants-lower-container">
+                    <div className="view-restaurants">
+                        {Array.isArray(this.state.restaurants.data) && this.state.restaurants.data.map( res => (
+                            <div className="restaurant-container" key={res.id}>
+                                <h4 className="restaurant-name">{res.name}</h4>
+                                <p className="restaurant-location">{res.location}</p>
+                                <p className="restaurant-cuisine">{res.cuisine}</p>
+                                <small className="restaurant-user-details">Added by {res.addedBy} on {moment(res.addedOn).format('MMMM Do YYYY')}</small>
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
         );
