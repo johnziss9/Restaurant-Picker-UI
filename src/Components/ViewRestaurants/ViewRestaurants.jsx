cc4cbfacab7732f6fe20cc0c5170f22f9393d7f3
John Z <johnziss9@gmail.com>
2021-03-10 21:19:26 +0000

    Changed port number for each endpoint to 8000.

--- src/Components/ViewRestaurants/ViewRestaurants.jsx
+++ src/Components/ViewRestaurants/ViewRestaurants.jsx
@@ -18,7 +18,7 @@ class ViewRestaurants extends React.Component {
 
     componentDidMount() {
         Promise.all([
-            fetch('https://localhost:5001/restaurant/GetNotVisited', {
+            fetch('https://localhost:8000/restaurant/GetNotVisited', {
                 method: 'get',
                 headers: {
                     'Accept': 'application/json',
@@ -27,7 +27,7 @@ class ViewRestaurants extends React.Component {
                 }
             })
             .then(res => res.json()),
-            fetch('https://localhost:5001/restaurant/GetVisited',  {
+            fetch('https://localhost:8000/restaurant/GetVisited',  {
                 method: 'get',
                 headers: {
                     'Accept': 'application/json',
